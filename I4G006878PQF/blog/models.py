61f5c6b38d8b8efc7a9caaae9fbde4ee03548a16
“Joshua <umukorojosh@gmail.com>
2022-07-04 08:33:35 +0100

    Creating a model

--- I4G006878PQF/blog/models.py
+++ I4G006878PQF/blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Post(models.Model):
+
+    # fields of the model
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_date = models.DateTimeField
+    published_date = models.DateTimeField
